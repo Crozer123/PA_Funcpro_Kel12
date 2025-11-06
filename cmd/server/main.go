4df93b2405afdb1baa054173ebabee5aa3aaac03
AHMAD <105153324+Dzox13524@users.noreply.github.com>
2025-11-06 21:35:00 +0700

    mentahan

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/Dzox13524/PA_Funcpro_Kel12/internal/middleware"
 )
 
 func main() {
-	http.HandleFunc("/ping", func (w http.ResponseWriter, r *http.Request){
+	// konfig comsole
+	log.SetFlags(0)
+
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /ping", func (w http.ResponseWriter, r *http.Request){
 		w.Header().Set("Content-Type", "application/json")
 		data := map[string]string{"data":"ping"}
 		json.NewEncoder(w).Encode(data)
 	}) 
-	http.HandleFunc("/pong", func (w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Content-Type", "application/json")
-		data := map[string]string{"data":"pong"}
-		json.NewEncoder(w).Encode(data)
-	}) 
-	http.ListenAndServe(":8080", nil);
+
+	var finalHandler http.Handler = mux
+	finalHandler = middleware.Logging(finalHandler)
+
+
+	http.ListenAndServe(":8080", finalHandler);
 }
---
+++ internal/middleware/logging.go
@@ -0,0 +1,31 @@
+package middleware
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+	"github.com/fatih/color"
+)
+
+func Logging(next http.Handler) http.Handler {
+	fn := func (w http.ResponseWriter, r *http.Request) {
+		warna := []*color.Color{
+			color.New(color.FgGreen),
+			color.New(color.FgMagenta),
+			color.New(color.FgCyan),
+			color.New(color.FgYellow),
+		}
+
+		if (r.URL.Path != "/favicon.ico" && r.URL.Path != "/.well-known/appspecific/com.chrome.devtools.json") {
+			waktu := time.Now().Format("15:04:05")
+			random := rand.New(rand.NewSource(time.Now().UnixNano()))
+			panjangSlice := len(warna)
+			indeksAcak := random.Intn(panjangSlice)
+			warnaTerpilih := warna[indeksAcak]
+			warnaTerpilih.Printf("[%s] -> IP: %s | PATH: %s | METOD: %s\n", waktu, r.RemoteAddr, r.URL.Path, r.Method)
+			next.ServeHTTP(w, r) 
+		}
+	}
+	return http.HandlerFunc(fn)
+
+}
